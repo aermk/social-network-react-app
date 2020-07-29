6adfdeec1994e89381032e15eee89f64a43b8362
Sasha <ermakovaas7@gmail.com>
2020-07-29 21:48:48 +0300

    init, (v14)

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Header.module.css'
+
+const Header = () => {
+  return(
+    <header className={styles.header}>
+        <img src='https://i7.pngflow.com/pngimage/7/671/png-cycling-racing-bicycle-computer-icons-road-bicycle-cycling-text-sport-racing-logo-clipart.png' />
+    </header >
+  )
+}
+
+export default Header;
