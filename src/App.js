436fb8ecd064c4625f111795d543e39219507cb0
Sasha <ermakovaas7@gmail.com>
2020-07-30 23:34:13 +0300

    added ruote, browser router v(19)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import React from 'react';
+import { Route, BrowserRouter } from 'react-router-dom'
 import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs';
+import News from './components/News/News';
+import Music from './components/Music/Music';
+import Settings from './components/Settings/Settings';
+
+// import { Route, BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
+
 
 
 const App = () => {
   return (
-    <div>
-      <div className='app-wrapper'>
-        <Header />
-        <Navbar />
-        <div className='app-wrapper-content'>
-          <Dialogs />
+    <BrowserRouter>
+      <div>
+        <div className='app-wrapper'>
+          <Header />
+          <Navbar />
+          <div className='app-wrapper-content'>
+            <Route path='/dialogs' component={Dialogs}/>
+            <Route path='/profile' component={Profile}/>
+            <Route path='/news' component={News}/>
+            <Route path='/music' component={Music}/>
+            <Route path='/settings' component={Settings}/>
+          </div>
         </div>
-        {/* <Profile /> */}
       </div>
-    </div>
+    </BrowserRouter>
     
   );
 }
