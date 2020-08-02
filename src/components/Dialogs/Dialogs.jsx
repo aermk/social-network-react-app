24fc4e6c6c779d65b9aa85ff99ef584bd4b6f344
Sasha <ermakovaas7@gmail.com>
2020-08-02 17:32:48 +0300

    added Dialogs Page (v21)

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,11 +4,21 @@ import styles from './Dialogs.module.css'
 
 const Dialogs = () => {
   return(
-    <div>
-      Dialogs
+      <div className ={styles.dialogs}>
+        <div className={styles.dialogsItems}> 
+            <div className={styles.dialog}>Sergey</div>
+            <div className={styles.dialog  + ' ' + styles.active}>Valentina</div>
+            <div className={styles.dialog}>Ivan</div>
+            <div className={styles.dialog}>Mom</div>
+            <div className={styles.dialog}>Dad</div>
+        </div>
+      <div className={styles.messages}>
+        <div className={styles.message}>Yo</div>
+        <div className={styles.message}>How are you?</div>
+        <div className={styles.message}>What are you doing today?</div>
+      </div>
     </div>
   )
-  
 }
 
 export default Dialogs;
